0293faa1f2e0abca21d317e25e0e3fa92cafd114
leezizon <99871097+leezizon@users.noreply.github.com>
2023-08-22 22:50:07 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index.html');
 });
 
+router.get('/lee', function(req, res, next) {
+  res.render('gpt2.html');
+});
+
 module.exports = router;
